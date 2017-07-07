195fe2b6615b3f75fffbb61e4d4efc1feb38a80d
pmmilewski <milewski.piotr.m@gmail.com>
2017-07-07 22:33:56 +0200

    Adding finding label by name and search in label scripts

--- search.py
+++ search.py
@@ -1,4 +1,4 @@
-# Script finds all labels matching given name fragment
+# Script finds all labels containing keyword or text fragment
 import sys
 import pandas as pd
 import re
@@ -6,6 +6,6 @@ import re
 if __name__=="__main__":
 	contents = pd.read_csv("data/CRDC2013_14content.csv", encoding="Latin-1")
 	name = sys.argv[1]
-	labels = contents[contents["NAME"].str.contains(name)]
+	labels = contents[contents["LABEL"].str.contains(name, case=False)]
 	pd.set_option('display.max_colwidth', -1)
 	print(labels[["NAME", "LABEL"]])
